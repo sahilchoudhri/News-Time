c641ce682dae66d091bd17a054d06d199bf0736d
Sahil <saahil999683@gmail.com>
2024-03-18 15:03:01 +0530

    alpha

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -9,7 +9,7 @@ function Cards(props) {
     const { data } = props;
     if (!data) {
         console.error("Cards received null data prop. This is a bug. Ignoring it.")
-        return <div>No data</div>
+        return <div className="text-slate-200 mt-[5rem] text-lg" >API is Paid hence can`t display news on live site.</div>
     }
 
     return (
@@ -18,7 +18,7 @@ function Cards(props) {
                 data.map((item, index) => {
                     if (!item) {
                         console.error(`Cards received null item at index ${index}. This is a bug. Ignoring it.`)
-                        return <div className='text-slate-200 mt-[5rem] text-lg ' >API is Paid hence can`t display news on live site.</div>
+                        return <div className='text-slate-200 mt-[5rem] text-lg ' >No Data</div>
                     }
                     return (
                         <Card key={index} item={item} />
